ab1b032435adabd009c03329feb308d05ed57ade
Samuel <samueligcorp@gmail.com>
2024-04-30 17:45:06 +0200

    Terminado

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+let jugador1;
+let jugador2;
+let turno = 1;
+
 $(document).ready(function() {
 
     $('#nombrespopup').css('display', 'flex');
 
     $('#start').click(function() {
-        let jugador1 = $('#jugador1').val();
-        let jugador2 = $('#jugador2').val();
-
+        jugador1 = $('#jugador1').val();
+        jugador2 = $('#jugador2').val();
+        
         $('#j1').text(jugador1);
         $('#j2').text(jugador2);
 
@@ -14,7 +18,7 @@ $(document).ready(function() {
     });
 
     let tablero = [[null,null,null],[null,null,null],[null,null,null]];
-    let turno = 0;
+    
 
     $(".imgcontainer").click(function() {
         if ($(this).children('img').attr('src') === 'Imagenes/white.svg') {
@@ -42,8 +46,14 @@ $(document).ready(function() {
         // Check rows
         for (let i = 0; i < 3; i++) {
             if (tablero[i][0] !== null && tablero[i][0] === tablero[i][1] && tablero[i][0] === tablero[i][2]) {
-                alert(tablero[i][0] + " wins!");
-                resetGame();
+
+                if (tablero[i][0]=="X") {
+                    showWinningPopup(jugador1);
+
+                }else{  
+                    showWinningPopup(jugador2);
+
+                }
                 return;
             }
         }
@@ -51,8 +61,13 @@ $(document).ready(function() {
         // Check columns
         for (let j = 0; j < 3; j++) {
             if (tablero[0][j] !== null && tablero[0][j] === tablero[1][j] && tablero[0][j] === tablero[2][j]) {
-                alert(tablero[0][j] + " wins!");
-                resetGame();
+                if (tablero[0][j]=="X") {
+                    showWinningPopup(jugador1);
+
+                }else{  
+                    showWinningPopup(jugador2);
+
+                }
                 return;
             }
         }
@@ -60,8 +75,12 @@ $(document).ready(function() {
         // Check diagonals
         if ((tablero[0][0] !== null && tablero[0][0] === tablero[1][1] && tablero[0][0] === tablero[2][2]) ||
             (tablero[0][2] !== null && tablero[0][2] === tablero[1][1] && tablero[0][2] === tablero[2][0])) {
-            alert(tablero[1][1] + " wins!");
-            resetGame();
+
+            if (tablero[1][1]=="X") {
+                showWinningPopup(jugador1);
+            }else{  
+                showWinningPopup(jugador2);
+            }
             return;
         }
 
@@ -77,25 +96,31 @@ $(document).ready(function() {
             if (!draw) break;
         }
         if (draw) {
-            alert("It's a draw!");
-            resetGame();
+            showEmpatePopup();
             return;
         }
     }
 
     function showWinningPopup(winner) {
-        $("#mensaje").text("El jugador"+ winner + " ha ganado!");
-        $("#backgroundmensaje").css("display", "flex");
+
+        $("#winerpopup").css("display", "flex");
+        $("#mensaje").text("El jugador "+ winner + " ha ganado!");
+    }
+    function showEmpatePopup() {
+        $("#felicitaciones").text("Oh no!")
+        $("#winerpopup").css("display", "flex");
+        $("#mensaje").text("Es un empate!");
     }
 
     // Close the popup when the user clicks on the close button
     $(".restartboton").click(function() {
-        $("#backgroundmensaje").css("display", "none");
+        resetGame();
+        $("#winerpopup").css("display", "none");
     });
 
     function resetGame() {
         $(".imgcontainer").children('img').attr('src',"Imagenes/white.svg");
         tablero = [[null,null,null],[null,null,null],[null,null,null]];
-        turno = 0;
+        turno = 1;
     }
 });
